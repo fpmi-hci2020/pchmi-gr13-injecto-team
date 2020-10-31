5878abaec4791e5b3fd783f5a5748275cfc57349
Sammaritan <pasha.frigin@gmail.com>
2020-10-31 20:50:38 +0300

    Add swagger

--- src/Injecto/Injecto.Web/Startup.cs
+++ src/Injecto/Injecto.Web/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Injecto.Web
 {
@@ -27,11 +28,35 @@ namespace Injecto.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Injecto API",
+                    Description = "A simple example Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vladislav Buzun",
+                        Email = "vladislav.buzun@gmail.com",
+                        Url = new Uri("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+                    }
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
